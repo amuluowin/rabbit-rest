640c9f4f0a2a8d007b8ae7320487bfaebcdc691e
albert <amuluowin@163.com>
2020-04-23 17:48:45 +0800

    update

--- src/RestTrait.php
+++ src/RestTrait.php
@@ -6,6 +6,7 @@ namespace Rabbit\Rest;
 use DI\DependencyException;
 use DI\NotFoundException;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\SimpleCache\CacheInterface;
 use rabbit\db\mysql\CreateExt;
 use rabbit\db\mysql\DeleteExt;
 use rabbit\db\mysql\Orm;
@@ -18,6 +19,13 @@ use rabbit\helper\ArrayHelper;
  */
 trait RestTrait
 {
+    /** @var CacheInterface */
+    protected $cache;
+    /** @var string */
+    protected $cacheHeader = 'Cache';
+    /** @var callable */
+    protected $cacheCallback;
+
     /**
      * @param array $params
      * @param ServerRequestInterface|null $request
@@ -26,14 +34,15 @@ trait RestTrait
      */
     public function __invoke(array $params = [], ServerRequestInterface $request = null, string $method = '')
     {
-        return $this->$method($params);
+        return $this->$method($params, $request);
     }
 
     /**
      * @param array $body
-     * @return array|array[]
+     * @param ServerRequestInterface|null $request
+     * @return array
      */
-    protected function create(array $body): array
+    protected function create(array $body, ServerRequestInterface $request = null): array
     {
         $model = new $this->modelClass();
         return $this->modelClass::getDb()->transaction(function () use ($model, &$body) {
@@ -43,9 +52,10 @@ trait RestTrait
 
     /**
      * @param array $body
+     * @param ServerRequestInterface|null $request
      * @return array
      */
-    protected function update(array $body): array
+    protected function update(array $body, ServerRequestInterface $request = null): array
     {
         $model = new $this->modelClass();
         return $this->modelClass::getDb()->transaction(function () use ($model, &$body) {
@@ -55,9 +65,10 @@ trait RestTrait
 
     /**
      * @param array $body
+     * @param ServerRequestInterface|null $request
      * @return mixed
      */
-    protected function delete(array $body)
+    protected function delete(array $body, ServerRequestInterface $request = null)
     {
         $model = new $this->modelClass();
         return $this->modelClass::getDb()->transaction(function () use ($model, &$body) {
@@ -67,38 +78,53 @@ trait RestTrait
 
     /**
      * @param array $filter
-     * @return mixed
-     * @throws DependencyException
-     * @throws NotFoundException
-     */
-    protected function list(array $filter): array
-    {
-        $filter = ArrayHelper::getValueByArray($filter, [0, 1], null, ['*', []]);
-        return Orm::search($this->modelClass::getDb(), $this->modelClass::tableName(), $filter);
-    }
-
-    /**
-     * @param array $filter
+     * @param ServerRequestInterface|null $request
      * @return array
      * @throws DependencyException
      * @throws NotFoundException
      */
-    protected function view(array $filter): array
+    protected function list(array $filter, ServerRequestInterface $request = null): array
     {
         $filter = ArrayHelper::getValueByArray($filter, [0, 1], null, ['*', []]);
-        return Orm::search($this->modelClass::getDb(), $this->modelClass::tableName(), $filter, 'queryOne');
+        $duration = -1;
+        if (is_callable($this->cacheCallback)) {
+            $duration = call_user_func($this->cacheCallback, $request);
+        }
+        return Orm::search($this->modelClass::getDb(), $this->modelClass::tableName(), $filter, 'queryAll', $duration === '' ? -1 : (int)$duration, $this->cache);
     }
 
     /**
      * @param array $filter
+     * @param ServerRequestInterface|null $request
+     * @return array
+     * @throws DependencyException
+     * @throws NotFoundException
+     */
+    protected function view(array $filter, ServerRequestInterface $request = null): array
+    {
+        $filter = ArrayHelper::getValueByArray($filter, [0, 1], null, ['*', []]);
+        $duration = -1;
+        if (is_callable($this->cacheCallback)) {
+            $duration = call_user_func($this->cacheCallback, $request);
+        }
+        return Orm::search($this->modelClass::getDb(), $this->modelClass::tableName(), $filter, 'queryOne', $duration === '' ? -1 : (int)$duration, $this->cache);
+    }
+
+    /**
+     * @param array $filter
+     * @param ServerRequestInterface|null $request
      * @return mixed
      * @throws DependencyException
      * @throws NotFoundException
      */
-    protected function search(array $filter)
+    protected function search(array $filter, ServerRequestInterface $request = null)
     {
         $method = ArrayHelper::remove($filter, 'method', 'queryAll');
         $filter = ArrayHelper::getValueByArray($filter['query'], [0, 1], null, ['*', []]);
-        return Orm::search($this->modelClass::getDb(), $this->modelClass::tableName(), $filter, $method);
+        $duration = -1;
+        if (is_callable($this->cacheCallback)) {
+            $duration = call_user_func($this->cacheCallback, $request);
+        }
+        return Orm::search($this->modelClass::getDb(), $this->modelClass::tableName(), $filter, $method, $duration === '' ? -1 : (int)$duration, $this->cache);
     }
 }
